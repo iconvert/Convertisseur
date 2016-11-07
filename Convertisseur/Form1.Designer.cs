c429a5b7b26e852f61c0f2f94300fa5b5f5c3797
Romain Fontaine <romain.fontaine@isen-lille.fr>
2016-11-07 14:20:56 +0100

    Initial commit

---
+++ Convertisseur/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnChiffre1 = new System.Windows.Forms.Button();
+            this.btnChiffre9 = new System.Windows.Forms.Button();
+            this.btnChiffre8 = new System.Windows.Forms.Button();
+            this.btnChiffre7 = new System.Windows.Forms.Button();
+            this.btnChiffre6 = new System.Windows.Forms.Button();
+            this.btnChiffre5 = new System.Windows.Forms.Button();
+            this.btnChiffre4 = new System.Windows.Forms.Button();
+            this.btnChiffre3 = new System.Windows.Forms.Button();
+            this.btnChiffre2 = new System.Windows.Forms.Button();
+            this.btnChiffre0 = new System.Windows.Forms.Button();
+            this.Type_unity = new System.Windows.Forms.ComboBox();
+            this.textBoxUnity1 = new System.Windows.Forms.TextBox();
+            this.textBoxUnity2 = new System.Windows.Forms.TextBox();
+            this.unity1 = new System.Windows.Forms.ComboBox();
+            this.unity2 = new System.Windows.Forms.ComboBox();
+            this.exchangePicture = new System.Windows.Forms.PictureBox();
+            this.btn_Return = new System.Windows.Forms.Button();
+            this.btn_CE = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.exchangePicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnChiffre1
+            // 
+            this.btnChiffre1.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnChiffre1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnChiffre1.Font = new System.Drawing.Font("Brush Script Std", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChiffre1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnChiffre1.Location = new System.Drawing.Point(46, 112);
+            this.btnChiffre1.Name = "btnChiffre1";
+            this.btnChiffre1.Size = new System.Drawing.Size(60, 60);
+            this.btnChiffre1.TabIndex = 0;
+            this.btnChiffre1.Text = "1";
+            this.btnChiffre1.UseVisualStyleBackColor = false;
+            this.btnChiffre1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btnChiffre9
+            // 
+            this.btnChiffre9.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnChiffre9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnChiffre9.Font = new System.Drawing.Font("Brush Script Std", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChiffre9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnChiffre9.Location = new System.Drawing.Point(176, 244);
+            this.btnChiffre9.Name = "btnChiffre9";
+            this.btnChiffre9.Size = new System.Drawing.Size(61, 60);
+            this.btnChiffre9.TabIndex = 1;
+            this.btnChiffre9.Text = "9";
+            this.btnChiffre9.UseVisualStyleBackColor = false;
+            // 
+            // btnChiffre8
+            // 
+            this.btnChiffre8.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnChiffre8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnChiffre8.Font = new System.Drawing.Font("Brush Script Std", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChiffre8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnChiffre8.Location = new System.Drawing.Point(111, 244);
+            this.btnChiffre8.Name = "btnChiffre8";
+            this.btnChiffre8.Size = new System.Drawing.Size(60, 60);
+            this.btnChiffre8.TabIndex = 2;
+            this.btnChiffre8.Text = "8";
+            this.btnChiffre8.UseVisualStyleBackColor = false;
+            // 
+            // btnChiffre7
+            // 
+            this.btnChiffre7.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnChiffre7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnChiffre7.Font = new System.Drawing.Font("Brush Script Std", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChiffre7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnChiffre7.Location = new System.Drawing.Point(46, 244);
+            this.btnChiffre7.Name = "btnChiffre7";
+            this.btnChiffre7.Size = new System.Drawing.Size(60, 60);
+            this.btnChiffre7.TabIndex = 3;
+            this.btnChiffre7.Text = "7";
+            this.btnChiffre7.UseVisualStyleBackColor = false;
+            this.btnChiffre7.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnChiffre6
+            // 
+            this.btnChiffre6.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnChiffre6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnChiffre6.Font = new System.Drawing.Font("Brush Script Std", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChiffre6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnChiffre6.Location = new System.Drawing.Point(177, 178);
+            this.btnChiffre6.Name = "btnChiffre6";
+            this.btnChiffre6.Size = new System.Drawing.Size(60, 60);
+            this.btnChiffre6.TabIndex = 4;
+            this.btnChiffre6.Text = "6";
+            this.btnChiffre6.UseVisualStyleBackColor = false;
+            // 
+            // btnChiffre5
+            // 
+            this.btnChiffre5.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnChiffre5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnChiffre5.Font = new System.Drawing.Font("Brush Script Std", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChiffre5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnChiffre5.Location = new System.Drawing.Point(111, 178);
+            this.btnChiffre5.Name = "btnChiffre5";
+            this.btnChiffre5.Size = new System.Drawing.Size(60, 60);
+            this.btnChiffre5.TabIndex = 5;
+            this.btnChiffre5.Text = "5";
+            this.btnChiffre5.UseVisualStyleBackColor = false;
+            // 
+            // btnChiffre4
+            // 
+            this.btnChiffre4.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnChiffre4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnChiffre4.Font = new System.Drawing.Font("Brush Script Std", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChiffre4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnChiffre4.Location = new System.Drawing.Point(45, 178);
+            this.btnChiffre4.Name = "btnChiffre4";
+            this.btnChiffre4.Size = new System.Drawing.Size(60, 60);
+            this.btnChiffre4.TabIndex = 6;
+            this.btnChiffre4.Text = "4";
+            this.btnChiffre4.UseVisualStyleBackColor = false;
+            // 
+            // btnChiffre3
+            // 
+            this.btnChiffre3.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnChiffre3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnChiffre3.Font = new System.Drawing.Font("Brush Script Std", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChiffre3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnChiffre3.Location = new System.Drawing.Point(177, 112);
+            this.btnChiffre3.Name = "btnChiffre3";
+            this.btnChiffre3.Size = new System.Drawing.Size(60, 60);
+            this.btnChiffre3.TabIndex = 7;
+            this.btnChiffre3.Text = "3";
+            this.btnChiffre3.UseVisualStyleBackColor = false;
+            // 
+            // btnChiffre2
+            // 
+            this.btnChiffre2.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnChiffre2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnChiffre2.Font = new System.Drawing.Font("Brush Script Std", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChiffre2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnChiffre2.Location = new System.Drawing.Point(111, 112);
+            this.btnChiffre2.Name = "btnChiffre2";
+            this.btnChiffre2.Size = new System.Drawing.Size(60, 60);
+            this.btnChiffre2.TabIndex = 8;
+            this.btnChiffre2.Text = "2";
+            this.btnChiffre2.UseVisualStyleBackColor = false;
+            // 
+            // btnChiffre0
+            // 
+            this.btnChiffre0.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnChiffre0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnChiffre0.Font = new System.Drawing.Font("Brush Script Std", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChiffre0.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnChiffre0.Location = new System.Drawing.Point(111, 310);
+            this.btnChiffre0.Name = "btnChiffre0";
+            this.btnChiffre0.Size = new System.Drawing.Size(60, 60);
+            this.btnChiffre0.TabIndex = 9;
+            this.btnChiffre0.Text = "0";
+            this.btnChiffre0.UseVisualStyleBackColor = false;
+            // 
+            // Type_unity
+            // 
+            this.Type_unity.FormattingEnabled = true;
+            this.Type_unity.Location = new System.Drawing.Point(91, 7);
+            this.Type_unity.Name = "Type_unity";
+            this.Type_unity.Size = new System.Drawing.Size(106, 21);
+            this.Type_unity.TabIndex = 10;
+            this.Type_unity.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBoxUnity1
+            // 
+            this.textBoxUnity1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxUnity1.Location = new System.Drawing.Point(5, 73);
+            this.textBoxUnity1.Name = "textBoxUnity1";
+            this.textBoxUnity1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxUnity1.TabIndex = 11;
+            // 
+            // textBoxUnity2
+            // 
+            this.textBoxUnity2.Location = new System.Drawing.Point(176, 73);
+            this.textBoxUnity2.Name = "textBoxUnity2";
+            this.textBoxUnity2.Size = new System.Drawing.Size(100, 20);
+            this.textBoxUnity2.TabIndex = 12;
+            this.textBoxUnity2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // unity1
+            // 
+            this.unity1.FormattingEnabled = true;
+            this.unity1.Location = new System.Drawing.Point(5, 46);
+            this.unity1.Name = "unity1";
+            this.unity1.Size = new System.Drawing.Size(100, 21);
+            this.unity1.TabIndex = 13;
+            // 
+            // unity2
+            // 
+            this.unity2.FormattingEnabled = true;
+            this.unity2.Location = new System.Drawing.Point(176, 45);
+            this.unity2.Name = "unity2";
+            this.unity2.Size = new System.Drawing.Size(100, 21);
+            this.unity2.TabIndex = 14;
+            this.unity2.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // exchangePicture
+            // 
+            this.exchangePicture.Image = ((System.Drawing.Image)(resources.GetObject("exchangePicture.Image")));
+            this.exchangePicture.Location = new System.Drawing.Point(111, 46);
+            this.exchangePicture.Name = "exchangePicture";
+            this.exchangePicture.Size = new System.Drawing.Size(59, 47);
+            this.exchangePicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.exchangePicture.TabIndex = 15;
+            this.exchangePicture.TabStop = false;
+            this.exchangePicture.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btn_Return
+            // 
+            this.btn_Return.BackColor = System.Drawing.Color.Gainsboro;
+            this.btn_Return.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Return.Font = new System.Drawing.Font("Brush Script Std", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Return.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btn_Return.Location = new System.Drawing.Point(176, 310);
+            this.btn_Return.Name = "btn_Return";
+            this.btn_Return.Size = new System.Drawing.Size(60, 60);
+            this.btn_Return.TabIndex = 16;
+            this.btn_Return.Text = "←";
+            this.btn_Return.UseVisualStyleBackColor = false;
+            this.btn_Return.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // btn_CE
+            // 
+            this.btn_CE.BackColor = System.Drawing.Color.Gainsboro;
+            this.btn_CE.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_CE.Font = new System.Drawing.Font("Brush Script Std", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_CE.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btn_CE.Location = new System.Drawing.Point(46, 310);
+            this.btn_CE.Name = "btn_CE";
+            this.btn_CE.Size = new System.Drawing.Size(60, 60);
+            this.btn_CE.TabIndex = 17;
+            this.btn_CE.Text = "CE";
+            this.btn_CE.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(286, 382);
+            this.Controls.Add(this.btn_CE);
+            this.Controls.Add(this.btn_Return);
+            this.Controls.Add(this.exchangePicture);
+            this.Controls.Add(this.unity2);
+            this.Controls.Add(this.unity1);
+            this.Controls.Add(this.textBoxUnity2);
+            this.Controls.Add(this.textBoxUnity1);
+            this.Controls.Add(this.Type_unity);
+            this.Controls.Add(this.btnChiffre0);
+            this.Controls.Add(this.btnChiffre2);
+            this.Controls.Add(this.btnChiffre3);
+            this.Controls.Add(this.btnChiffre4);
+            this.Controls.Add(this.btnChiffre5);
+            this.Controls.Add(this.btnChiffre6);
+            this.Controls.Add(this.btnChiffre7);
+            this.Controls.Add(this.btnChiffre8);
+            this.Controls.Add(this.btnChiffre9);
+            this.Controls.Add(this.btnChiffre1);
+            this.Name = "Form1";
+            this.Text = "Convertisseur d\'unités";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.exchangePicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChiffre1;
+        private System.Windows.Forms.Button btnChiffre9;
+        private System.Windows.Forms.Button btnChiffre8;
+        private System.Windows.Forms.Button btnChiffre7;
+        private System.Windows.Forms.Button btnChiffre6;
+        private System.Windows.Forms.Button btnChiffre5;
+        private System.Windows.Forms.Button btnChiffre4;
+        private System.Windows.Forms.Button btnChiffre3;
+        private System.Windows.Forms.Button btnChiffre2;
+        private System.Windows.Forms.Button btnChiffre0;
+        private System.Windows.Forms.ComboBox Type_unity;
+        private System.Windows.Forms.TextBox textBoxUnity1;
+        private System.Windows.Forms.TextBox textBoxUnity2;
+        private System.Windows.Forms.ComboBox unity1;
+        private System.Windows.Forms.ComboBox unity2;
+        private System.Windows.Forms.PictureBox exchangePicture;
+        private System.Windows.Forms.Button btn_Return;
+        private System.Windows.Forms.Button btn_CE;
+    }
+}
+
